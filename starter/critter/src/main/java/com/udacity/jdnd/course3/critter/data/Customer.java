d2e3b472762d449b300deb5c34b4edbab108ff3e
Amol Jadhav <amol@Amols-MacBook-Air.local>
2021-07-03 14:14:08 +0530

    Added customer class

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/data/Customer.java
@@ -0,0 +1,58 @@
+package com.udacity.jdnd.course3.critter.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+    private String phoneNumber;
+    private String notes;
+
+    public Customer(String name, String phoneNumber, String notes) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.notes = notes;
+    }
+
+    public Customer(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
